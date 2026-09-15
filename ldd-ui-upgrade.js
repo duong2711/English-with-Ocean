ff23f58a27836c27287841eb7fe379999cfca094
duong2711 <147268801+duong2711@users.noreply.github.com>
2026-09-15 21:38:19 +0700

    Load reset and unlock countdown UI

--- ldd-ui-upgrade.js
+++ ldd-ui-upgrade.js
@@ -24,8 +24,10 @@
         ensureStylesheet('ldd-roadmap-news-style', 'ldd-ui-roadmap-news.css?v=2');
         ensureStylesheet('ldd-vocab-grammar-style', 'ldd-ui-vocab-grammar.css?v=1');
         ensureStylesheet('ldd-home-phonetics-style', 'ldd-ui-home-phonetics.css?v=1');
+        ensureStylesheet('ldd-timers-style', 'ldd-ui-timers.css?v=1');
         ensureScript('ldd-vocab-grammar-script', 'ldd-ui-vocab-grammar.js?v=2');
         ensureScript('ldd-home-phonetics-script', 'ldd-ui-home-phonetics.js?v=6-stable');
+        ensureScript('ldd-timers-script', 'ldd-ui-timers.js?v=1');
     }
 
     function ensureStylesheet(id, href) {
