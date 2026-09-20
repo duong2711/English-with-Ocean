b5968d8a0d22d4504c02e64b3826175b52c93a10
duong2711 <147268801+duong2711@users.noreply.github.com>
2026-09-20 12:33:32 +0700

    Reload Home branding copy

--- ldd-ui-upgrade.js
+++ ldd-ui-upgrade.js
@@ -35,7 +35,7 @@
         ensureScript('ldd-vocab-dedup-script', 'ldd-vocab-dedup.js?v=1');
         ensureScript('ldd-pronunciation-fix-script', 'ldd-pronunciation-fix.js?v=3');
         ensureScript('ldd-vocab-grammar-script', 'ldd-ui-vocab-grammar.js?v=3');
-        ensureScript('ldd-home-phonetics-script', 'ldd-ui-home-phonetics.js?v=8.8');
+        ensureScript('ldd-home-phonetics-script', 'ldd-ui-home-phonetics.js?v=8.9');
         ensureScript('ldd-student-grade-script', 'ldd-student-grade.js?v=2');
         ensureScript('ldd-thcs-vocab-reset-script', 'ldd-thcs-vocab-reset.js?v=3');
         ensureScript('ldd-fast-progress-script', 'ldd-fast-progress.js?v=1');
